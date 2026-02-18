e4de78e286bba9cce47afab0c5038995bd25fb03
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2026-02-18 10:06:51 -0500

    bases teóricas de oop

---
+++ bases.py
@@ -0,0 +1,26 @@
+#   CLASES
+# Crean objetos, definen propiedades(atributos) y comportamientos(métodos) que caracterizan a los objetos de un tipo específico. Al encapsular datos permiten la reutilización del código
+
+#   MÉTODOS
+# Son bloques de código que encapsulan una serie de acciones o comportamientos específicos asociados a un objeto, modificar su estado interno y responder a eventos dentro de un programa. Los métodos pueden acceder a los atributos de los objetos
+
+#   ATRIBUTOS
+# Son las caracteristicas o datos que describen el estado de un objeto en un contexto determinado. Estos pueden representar desde valores simples como valores simples hasta estructuras más complejas como objetos u otros tipos de datos. Los atributos definen las propiedades de un objeto las cuales se gestiona el estado del mismo
+
+#   OBJETOS
+# Son instancias específicas de una clase que encapsulan datos y comportamientos relacionados. Los objetos permiten modelar entidades del mundo real de manera modular y reutilizable, facilitando la organización y el diseño de sistemas de software eficientes y mantenibles
+
+#   ABSTRACCIÓN
+# Permite representar entidades del mundo real como objetos con características y comportamientos relevantes para el problema que se está resolviendo. Esto siplifica la complejidad del sistema al enfocarse en aspectos esencialesy porporcionar una dorma de modelar conceptos del mundo real en el código.
+
+#   ENCAPSULAMIENTO
+# Los detalles internos de un objeto deben estar ocultos fuera de su definición y solo deben ser accesibles a través de una interfaz claramente definida. Esto promueve la seguridad y la integridad de los datos al prevenir accesos no autorizados y facilida el mantenimiento del código.
+
+#   HERENCIA
+# permite que una clase(subclase) herede atributos y métodos de otra clase(superclase), lo que promueve la reutilziación del código y la organización jerárquica de las clases. Esto facilita la creación de nuevas clases que extienden el comportamiento de clases existentes, permitiendo mayor flexibilidad y modularidad en el diseño del sistema.
+
+#   POLIMORFISMO
+# Se refiere a la capacidad de objetos de diferentes clases a responder al mismo mensaje de manera diferente. En otras palabras, un mismmo método o mensaje puede producir diferentes resultados segun el tipo de objeto que lo reciba. Esto permite escribir código más genérico y reutilizable.
+
+#   ----------CONVENCIONES
+# los atributos que comienzan con un guion bajo _ se consideran protegidos (no deben ser accedidos directamente), y los que comienzan con doble guion bajo __ son privados (name mangling)
